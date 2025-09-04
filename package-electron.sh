efc73a9a2a68a27b2f944ed0c9bd096efbae4fd0
CatLover <152669316+catloversg@users.noreply.github.com>
2025-09-04 23:48:54 +0700

    Test

--- package-electron.sh
+++ package-electron.sh
@@ -16,6 +16,8 @@ BUILD_PLATFORM="${1:-"all"}"
 case $BUILD_PLATFORM in
   "win")
     electron-packager .package test-electron-packager --platform win32 --arch x64 --out .build --overwrite;;
+  "linux")
+    electron-packager .package test-electron-packager --platform linux --arch x64 --out .build --overwrite;;
   "mac")
     electron-packager .package test-electron-packager --platform darwin --arch universal --osx-universal.x64ArchFiles="Contents/Resources/app/node_modules/@catloversg/steamworks.js/dist/osx/*" --out .build --overwrite;;
 esac
